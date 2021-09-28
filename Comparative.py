6beba12f65effd6b22e4ee70bb2427c816caa3cd
surya-lights <surya.klights@gmail.com>
2021-09-28 18:55:26 +0530

    Commit Comparative

---
+++ Comparative.py
@@ -0,0 +1,20 @@
+a = 10
+b = 20
+
+# if two operands values are equal,then the condition becomes true.
+print(a == b)
+
+# if two operands values are not equal,then the condition becomes true.
+print(a != b)
+
+# if the value of left operand is greater than the value of right operand, then condition becomes true.
+print(a > b)
+
+# if the value of right operand is less than the value of right operand, then condition becomes true.
+print(a < b)
+
+# if the value of left operand is greater than or equal to the value of right operand, then the condition becomes true.
+print(a >= b)
+
+# if the value of right operand is greater than or equal to the value of left operand, then condition becomes true.
+print(a <= b)
