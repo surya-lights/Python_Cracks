12ca097ba2a4f57b4be3c96568f09f3b22d8b099
surya-lights <surya.klights@gmail.com>
2021-09-28 19:10:15 +0530

    Commit List

---
+++ List.py
@@ -0,0 +1,15 @@
+# Print list , declared in variable
+courses = {'English', 'Math', 'Biology', 'Science'}
+print(courses)
+# Length function
+courses = {'English', 'Math', 'Biology', 'Science'}
+print(len(courses))
+# To know the specific index value list
+courses = ["English", "Math", "Biology", "Science"]
+print(courses[3])
+# To make start and stop index value list
+courses = ["English", "Math", "Biology", "Science"]
+print(courses[0:2])
+# Call particular start index list
+courses = ["English", "Math", "Biology", "Science"]
+print(courses[2:])
