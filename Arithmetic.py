dacbe0fecb9c8494b2955cea1a069a68fbb72e76
surya-lights <surya.klights@gmail.com>
2021-09-28 18:25:24 +0530

    Commit Arithmetic

---
+++ Arithmetic.py
@@ -0,0 +1,48 @@
+val1 = 2
+val2 = 3
+
+# using the addition operator
+count = val1 + val2
+print(count)
+
+num1 = 5
+num2 = 3
+
+# using the subtraction operator
+res = num1 - num2
+print(res)
+
+num_1 = 2
+num_2 = 3
+
+# using the multiplication operator
+a = num_1 * num_2
+print(a)
+
+a1 = 3
+a2 = 2
+
+# using the division operator
+c = a1 / a2
+print(c)
+
+b1 = 3
+b2 = 2
+
+# using the modulus operator
+d = b1 % b2
+print(d)
+
+c1 = 2
+c2 = 3
+
+# using the exponentiation operator
+e = c1 ** c2
+print(e)
+
+f1 = 3
+f2 = 2
+
+# using the floor division
+g = f1 // f2
+print(g)
