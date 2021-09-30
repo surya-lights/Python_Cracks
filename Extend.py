7e965cdf3f5474064897cfd35c6fd72a41fc4228
surya-lights <surya.klights@gmail.com>
2021-09-30 15:13:35 +0530

    Commit Extend

---
+++ Extend.py
@@ -0,0 +1,9 @@
+# A list
+even_numbers = [4, 6, 8]
+
+# Another list
+numbers = [2]
+
+# Adding elements of even_numbers to numbers
+numbers.extend(even_numbers)
+print('List after extend():', numbers)
