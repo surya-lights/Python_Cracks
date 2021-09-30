0638189278a2022369afa1ea10917bf877f80aa4
surya-lights <surya.klights@gmail.com>
2021-09-30 15:38:08 +0530

    Commit remove

---
+++ List_remove.py
@@ -0,0 +1,9 @@
+# removes 'a' from list1
+list1 = ['a', 'b', 'c', 'd']
+list1.remove('a')
+print(list1)
+
+# the first presence of 1 is removed from the list
+list2 = [1, 2, 1, 1, 4, 5]
+list2.remove(1)
+print(list2)
