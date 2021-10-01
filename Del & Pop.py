fe44f559dc4027b85697aa655edc77e0a6b15757
surya-lights <surya.klights@gmail.com>
2021-10-01 11:16:39 +0530

    Commit Del and Pop

---
+++ Del & Pop.py
@@ -0,0 +1,11 @@
+# Delete method
+
+student = {'name': 'charles', 'age': 25, 'course': ['math', 'Biology']}
+del student['age']
+print(student)
+
+# Pop Method
+student = {'name': 'charles', 'age': 25, 'course': ['math', 'Biology']}
+age = student.pop('age')
+print(student)
+print(age)
