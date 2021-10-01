27fa07ca9d831aa6afcb7957b57bc23279ae9d60
surya-lights <surya.klights@gmail.com>
2021-10-01 12:33:17 +0530

    Commit Conditional

---
+++ Conditional.py
@@ -0,0 +1,20 @@
+# If condition was true
+language = 'Python'
+if language == "Python":
+    print('The Conditional was True')
+
+# Elif Statement
+a = 100
+b = 100
+if b > a:
+    print('b is greater than a')
+elif a == b:
+    print('a and b are equal')
+else:
+    print('a is greater than b')
+
+# If else statement
+if language == "Java":
+    print('Language is python')
+else:
+    print('No match')
