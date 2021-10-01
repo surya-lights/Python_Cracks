7ef2040ea568516b95494183c39baf0074aad9ba
surya-lights <surya.klights@gmail.com>
2021-10-01 11:26:36 +0530

    Commit keys_lists_items

---
+++ Key Func.py
@@ -0,0 +1,11 @@
+# Keys
+student = {'name': 'charles', 'age': 25, 'course': ['math', 'Biology']}
+print(student. keys())
+
+# To show values
+student = {'name': 'charles', 'age': 25, 'course': ['math', 'Biology']}
+print(student. values())
+
+# To display items
+student = {'name': 'charles', 'age': 25, 'course': ['math', 'Biology']}
+print(student. items())
