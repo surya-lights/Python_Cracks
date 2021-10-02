d4e632bb8b5bca887b46d9f2d98d217917329e09
surya-lights <surya.klights@gmail.com>
2021-10-02 14:33:30 +0530

    Commit String slice

---
+++ Slicing string.py
@@ -0,0 +1,16 @@
+
+# Reverse url
+sample_url = 'http://coreyms.com'
+print(sample_url[::-1])
+
+# To get top level domain
+sample_url = 'http://coreyms.com'
+print(sample_url[-4:])
+
+# Print url without http://
+sample_url = 'http://coreyms.com'
+print(sample_url[7:])
+
+# Without http:// or top level of domain
+sample_url = 'http://coreyms.com'
+print(sample_url[7:-4])
