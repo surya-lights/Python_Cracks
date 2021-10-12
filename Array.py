1aa70601842cc51fec7cf78ee8f8a05cc70da54a
surya-lights <surya.klights@gmail.com>
2021-10-12 19:50:51 +0530

    Commit Array

---
+++ Array.py
@@ -0,0 +1,10 @@
+
+# To create an array of numeric values, we need to import the array module.
+import array as arr
+a = arr.array('d', [1.1, 3.5, 4.5])
+print(a)
+
+# To loop through all the elements of an array
+cars = ["Ford", "Volvo", "BMW"]
+for x in cars:
+    print(x)
