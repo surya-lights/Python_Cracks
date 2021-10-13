737e80dae95c4f6e0540ba98a30eaaeb06415888
surya-lights <surya.klights@gmail.com>
2021-10-13 19:53:32 +0530

    Commit object

---
+++ Objects.py
@@ -0,0 +1,14 @@
+# Let us create a method in the Person class
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def myfunc(self):
+        print("Hello my name is " + self.name)
+
+
+p1 = Person("John", 35)
+p1.myfunc()
