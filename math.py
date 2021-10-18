9843b5a31ce22091a3e039b760ab28e328fdaf3d
surya-lights <surya.klights@gmail.com>
2021-10-18 19:45:50 +0530

    Commit math

---
+++ math.py
@@ -0,0 +1,14 @@
+# To find the highest or lowest value in an iteration
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+# To get the positive value of specified number using abs() function
+x = abs(-98.3)
+print(x)
+
+# Return the value of 5 to the power of 4 is (same as 5*5*5*5):
+x = pow(5, 4)
+print(x)
