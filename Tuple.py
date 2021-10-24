a6cfd889ffc78fa57175b3b4df79e562e55bf906
surya-lights <surya.klights@gmail.com>
2021-10-24 16:00:29 +0530

    Commit Tuple

---
+++ Tuple.py
@@ -0,0 +1,13 @@
+# Tuple items - data types
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+print(tuple1)
+print(tuple2)
+print(tuple3)
+
+# A Tuple with multiple data types
+tuple1 = ("abc", 34, True, 40, "male")
+
+print(tuple1)
