eea8a78d719f444e7a8fb481d7606371edcbf745
surya-lights <surya.klights@gmail.com>
2021-10-25 18:36:20 +0530

    Commit Random

--- Random.py
+++ Random.py
@@ -2,3 +2,24 @@
 import random
 
 print(random.randrange(1, 100))
+# Conversion of numbers
+x = 5    # int
+y = 7.2  # float
+z = 4j   # complex
+
+# convert from int to float:
+a = float(x)
+
+# convert from float to int:
+b = int(y)
+
+# convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
