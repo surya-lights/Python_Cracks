b2a45176818d7704fd11bbff912c697befb12f83
surya-lights <surya.klights@gmail.com>
2021-10-28 17:50:07 +0530

    Commit Data Conversion

---
+++ Data type Conversion.py
@@ -0,0 +1,17 @@
+# conversion of int to float
+x = float(8)
+
+
+# conversion of float to int
+y = int(5.3)
+
+# conversion int to complex
+z = complex(x)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
